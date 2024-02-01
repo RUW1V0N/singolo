ffdb880c808c7b8a7945e9f18b5162c0a96fc973
Yauheni Ulitsenak <von1we98@gmail.com>
2024-02-01 20:50:43 +0100

    added image shuffling and image selection

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function mixImg(){
-    const picturesPortfolioContainer = document.querySelector(".picturesPortfolio .imgWrapper");
-    const imgItems = [...document.querySelectorAll(".imgPortfolio")];
-    imgItems.sort(() => Math.random() - 0.5);
-    picturesPortfolioContainer.innerHTML = "";
-    imgItems.forEach((element) => {
-        picturesPortfolioContainer.appendChild(element);
-    });
+function portfolioButtonClickHandler(event) {
+  const buttonsPortfolio = [...document.querySelectorAll(".buttonPortfolio")];
+  const target = event.target;
+  const focusButtonPortfolio = "focusButtonPortfolio";
+  if (target.classList.contains(focusButtonPortfolio)) {
+    return;
+  }
+  buttonsPortfolio.forEach((element) => {
+    element.classList.remove(focusButtonPortfolio);
+  });
+  target.classList.add(focusButtonPortfolio);
+  mixImg();
 }
 
-document.querySelector(".buttonWrapper").addEventListener("click", mixImg);
+function mixImg() {
+  const picturesPortfolioContainer = document.querySelector(
+    ".picturesPortfolio .imgWrapper"
+  );
+  const imgItems = [...document.querySelectorAll(".imgPortfolio")];
+  imgItems.sort(() => Math.random() - 0.5);
+  picturesPortfolioContainer.innerHTML = "";
+  imgItems.forEach((element) => {
+    picturesPortfolioContainer.appendChild(element);
+  });
+}
+
+document
+  .querySelector(".buttonWrapper")
+  .addEventListener("click", portfolioButtonClickHandler);
+
+function selectImage(event) {
+  const imgPortfolio = [...document.querySelectorAll(".focusImgPortfolio")];
+  const target = event.target;
+  const focusImgPortfolio = "focusImgPortfolio";
+  target.classList.toggle(focusImgPortfolio);
+  if (imgPortfolio[0]) {
+    imgPortfolio[0].classList.remove(focusImgPortfolio);
+  }
+}
+
+document.querySelector(".imgWrapper").addEventListener("click", selectImage);
